32cca1acd4326365527c1f76eaa95f179069bd90
Eric-Home <ericoriez@live.fr>
2026-03-05 18:03:07 +0100

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
       ],
 
       link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
